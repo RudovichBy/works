27282a90b149db95d0c3026a1904c8cd94e3379c
Oleg Rudovich <olegtor11@gmail.com>
2020-12-01 00:22:49 +0300

    дradiobazar v0.6.5

--- radiobazaar/src/js/main.js
+++ radiobazaar/src/js/main.js
@@ -43,10 +43,10 @@ window.addEventListener('DOMContentLoaded', () => {
 	let mySwiper = new Swiper('.swiper-container', {
 		loop: true,
 		speed: 800,
-		autoplay: {
-			delay: 3000,
-			disableOnInteraction: false,
-		},
+		// autoplay: {
+		// 	delay: 3000,
+		// 	disableOnInteraction: false,
+		// },
 		pagination: {
 			el: '.swiper-pagination',
 			clickable: true,
