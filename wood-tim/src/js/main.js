ef39d20fb0aed90c9c4b8176a588076ee61ea4cb
Oleg Rudovich <olegtor11@gmail.com>
2022-01-17 00:03:59 +0300

    страница описания товара табы

--- wood-tim/src/js/main.js
+++ wood-tim/src/js/main.js
@@ -1,6 +1,7 @@
 import scrollingicator from './modules/scrollingicator';
 import burgerMenu from './modules/burgerMenu';
 import scrolling from './modules/scrolling';
+import tabs from './modules/tabs';
 // import sliderSwiper from './modules/slider';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,6 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	scrollingicator();
     burgerMenu('.mobile_menu', '.burger-menu_button', '.nav_item', '.burger-menu_overlay');
     scrolling('.pageup');
+    tabs('.product ', '.product_toggle', '.product_toggle-collum', 'product_toggle-active');
 
 	let mySwiper = new Swiper('.swiper', {
         loop: true,
